aeb3fb48fa69cb61e5960ece0634f4b8b01b1a13
Juhana Autti <ext-juhana.autti@3stepit.com>
2025-05-12 12:12:27 +0300

    add quality consts

---
+++ frontend/src/utils/quality.js
@@ -0,0 +1,53 @@
+export const craftingQualityOptions = [
+  {
+    name: "Normal",
+    value: "common",
+  },
+  {
+    name: "Good",
+    value: "uncommon",
+  },
+  {
+    name: "Great",
+    value: "rare",
+  },
+  {
+    name: "Excellent",
+    value: "epic",
+  },
+  {
+    name: "Perfect",
+    value: "legendary",
+  },
+  {
+    name: "Eternal",
+    value: "ethereal",
+  },
+];
+
+export const qualityOptions = [
+  {
+    name: "Common",
+    value: "common",
+  },
+  {
+    name: "Uncommon",
+    value: "uncommon",
+  },
+  {
+    name: "Rare",
+    value: "rare",
+  },
+  {
+    name: "Epic",
+    value: "epic",
+  },
+  {
+    name: "Legendary",
+    value: "legendary",
+  },
+  {
+    name: "Ethereal",
+    value: "ethereal",
+  },
+];
