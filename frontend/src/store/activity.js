f2cd62c0694ae757ce1de33350df1a4c572222ab
Juhana Autti <ext-juhana.autti@3stepit.com>
2025-06-01 13:49:00 +0300

    clean up activityStore

--- frontend/src/store/activity.js
+++ frontend/src/store/activity.js
@@ -3,23 +3,9 @@ import { getActivity } from "@/utils/axios/api_routes";
 
 export const useActivityStore = defineStore("activity", {
   state: () => ({
-    skill: null,
-    level: 0,
     activity: null,
   }),
-  getters: {
-    activitySelected: (state) =>
-      state.activity &&
-      state.activity.name &&
-      state.activity.name.toLowerCase() !== "none",
-  },
   actions: {
-    setSkill(skill) {
-      this.skill = skill;
-    },
-    setLevel(level) {
-      this.level = level;
-    },
     setActivity(activity) {
       this.activity = activity;
     },
