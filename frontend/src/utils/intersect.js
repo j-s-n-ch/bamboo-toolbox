82166fefcb079616fbcdedfe855862c58046be7e
Juhana Autti <ext-juhana.autti@3stepit.com>
2025-06-02 09:48:28 +0300

    add intersect util

---
+++ frontend/src/utils/intersect.js
@@ -0,0 +1,6 @@
+export function intersect(a, b) {
+  var setA = new Set(a);
+  var setB = new Set(b);
+  var intersection = new Set([...setA].filter(x => setB.has(x)));
+  return Array.from(intersection);
+}
