88abfa0fb45e4796697e1aa3e4e5a16d992f8135
Juhana Autti <juhana.autti@gmail.com>
2025-06-30 15:48:00 +0300

    add recipe loading

--- frontend/src/store/activity.js
+++ frontend/src/store/activity.js
@@ -1,16 +1,21 @@
 import { defineStore } from "pinia";
 import {
   getActivity,
+  getRecipe,
   searchLocations,
   getActivities,
+  getRecipes,
   getKeywords,
 } from "@/utils/axios/api_routes";
+import { activityNone } from "@/utils/activityNone";
 
 export const useActivityStore = defineStore("activity", {
   state: () => ({
     activities: [],
+    recipes: [],
     keywords: [],
     activity: null,
+    recipe: null,
     location: null,
     locations: null,
     showCombined: true,
@@ -20,25 +25,33 @@ export const useActivityStore = defineStore("activity", {
     activitySelected: (state) => {
       return state.activity !== null && state.activity.id !== "activity-none";
     },
+    recipeSelected: (state) => {
+      return state.recipe !== null && state.recipe.id !== "activity-none";
+    },
   },
   actions: {
     async fetchActivitiesData() {
       if (this.isLoaded) return;
 
-      const [{ data: activities }, { data: keywords }] = await Promise.all([
-        getActivities(),
-        getKeywords(),
-      ]);
+      const [{ data: activities }, { data: recipes }, { data: keywords }] =
+        await Promise.all([getActivities(), getRecipes(), getKeywords()]);
 
-      this.keywords = keywords;
       this.activities = activities;
+      this.keywords = keywords;
+      this.recipes = recipes;
 
       this.isLoaded = true;
     },
 
     setActivity(activity) {
+      this.recipe = activityNone;
       this.activity = { ...activity, value: activity.name };
     },
+    setRecipe(recipe) {
+      this.activity = activityNone;
+      this.recipe = { ...recipe, value: recipe.name };
+    },
+
     setLocations(locations) {
       this.locations = locations;
     },
@@ -53,6 +66,10 @@ export const useActivityStore = defineStore("activity", {
       const { data: locations } = await searchLocations({ activityList: id });
       this.setLocations(locations);
       if (locations.length) this.setLocation(locations[0]);
+    },
+    async loadRecipe(id) {
+      const { data: recipe } = await getRecipe({ id });
+      this.setRecipe(recipe);
     },
   },
 });
