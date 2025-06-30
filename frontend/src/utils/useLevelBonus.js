55bf72e5e1ba3b0c5e4eb3466117f88c871de48d
Juhana Autti <juhana.autti@gmail.com>
2025-06-30 16:20:28 +0300

    fix WE bonus for activities and recipes

--- frontend/src/utils/useLevelBonus.js
+++ frontend/src/utils/useLevelBonus.js
@@ -1,18 +1,36 @@
 import { computed } from "vue";
 import { useActivityStore } from "@/store/activity";
 import { usePlayerStore } from "@/store/player";
+import { useItemsStore } from "@/store/items";
 
 export function useLevelBonus() {
   const activityStore = useActivityStore();
   const playerStore = usePlayerStore();
+  const itemStore = useItemsStore();
+
+  const activityLevelRequirement = (activity, skill) =>
+    activity.levelRequirementsMap[skill];
+
+  const recipeLevelRequirement = (recipe) =>
+    recipe.requirements
+      .map(({ requirement }) => requirement)
+      .filter(
+        ({ type, requirement }) =>
+          type === "skillLevel" && requirement.skill === skill
+      )?.[0] || 1;
 
   const workEfficiencyBonus = computed(() => {
-    if (!activityStore.activitySelected || !activityStore.recipeSelected)
+    if (!activityStore.activitySelected && !activityStore.recipeSelected)
       return null;
-    const [mainSkill, ..._] = activityStore.activity.relatedSkillsList;
-    const levelRequirement =
-      activityStore.activity.levelRequirementsMap[mainSkill];
-    const playerLevel = playerStore.skillLevels[mainSkill] || 1;
+    const isActivity = activityStore.activitySelected;
+    const activity = isActivity ? activityStore.activity : activityStore.recipe;
+    const [skill] = isActivity
+      ? activity.relatedSkillsList
+      : activity.relatedSkills;
+    const levelRequirement = isActivity
+      ? activityLevelRequirement(activity, skill)
+      : recipeLevelRequirement(activity);
+    const playerLevel = playerStore.skillLevels[skill] || 1;
 
     const levelDiff = Math.min(20, Math.max(playerLevel - levelRequirement, 0));
     const value = levelDiff * 0.0125;
