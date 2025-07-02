b0db24e8e33ed90778377e1b8787aac8f3ca0b7e
Juhana Autti <juhana.autti@gmail.com>
2025-07-02 18:59:28 +0300

    install vue-router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,19 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { router } from "./routes";
 import directives from "@/directives";
 import App from "./App.vue";
 
-const app = createApp(App);
 const pinia = createPinia();
 
+const app = createApp(App);
 app.use(pinia);
+app.use(router);
 
 Object.entries(directives).forEach(([name, directive]) => {
   app.directive(name, directive);
 });
 
-app.mount("#app");
+router.isReady().then(() => {
+  app.mount("#app");
+});
