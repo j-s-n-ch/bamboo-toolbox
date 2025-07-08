996960b95667c987ff883d6764ccbc993a16f19f
Juhana Autti <juhana.autti@gmail.com>
2025-07-08 08:05:50 +0300

    add nav and about page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,6 +10,7 @@ import Hub from "./components/hub/Hub.vue";
 import Activity from "./components/activity/Activity.vue";
 import Gear from "./components/gear/Gear.vue";
 import Footer from "./components/footer/Footer.vue";
+import About from "./components/about/About.vue";
 import LoadingThrobber from "./components/common/LoadingThrobber.vue";
 
 const urlStore = useUrlStore();
@@ -21,9 +22,8 @@ const checkScreenSize = () => {
   isMobile.value = window.innerWidth <= 768;
 };
 
-const tabs = { Hub, Gear, Activity };
-
-// Refs for each tab panel
+const contentTabs = { Hub, Gear, Activity };
+const tabs = { About, ...contentTabs };
 const tabRefs = reactive({});
 
 function scrollToTab(tabName) {
@@ -32,8 +32,17 @@ function scrollToTab(tabName) {
     const el = tabRefs[tabName];
     if (el && el.scrollIntoView) {
       el.scrollIntoView({ behavior: "instant", block: "start" });
+      window.scrollBy({ top: -40, left: 0, behavior: "instant" });
     }
   });
+}
+
+function shouldShowTab(tabName) {
+  if (isMobile.value) {
+    return activeTab.value === tabName;
+  } else {
+    return activeTab.value !== "About";
+  }
 }
 
 const bootstrap = async () => {
@@ -71,20 +80,38 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <loading-throbber v-if="!isLoaded" />
+  <header class="main-header">
+    <a
+      href="#"
+      @click.prevent="scrollToTab('About')"
+      :ref="(el) => (tabRefs['About'] = el)"
+      :class="{ active: activeTab === 'About' }"
+      >About</a
+    >
+  </header>
+  <loading-throbber v-if="!isLoaded" class="throbber" />
   <div v-else :class="isMobile ? 'mobile-layout' : 'desktop-layout'">
     <div
-      v-for="tabName in Object.keys(tabs)"
+      v-show="activeTab === 'About'"
+      :class="['tab-panel', { active: activeTab === 'About' }]"
+      :tabindex="isMobile ? 0 : undefined"
+    >
+      <About @back="scrollToTab('Hub')" />
+    </div>
+
+    <div
+      v-for="tabName in Object.keys(contentTabs)"
       :key="tabName"
       :ref="(el) => (tabRefs[tabName] = el)"
       :class="['tab-panel', { active: activeTab === tabName }]"
       :tabindex="isMobile ? 0 : undefined"
+      v-show="shouldShowTab(tabName)"
     >
       <component :is="tabs[tabName]" />
     </div>
     <Footer
       v-if="isMobile"
-      :tabs="tabs"
+      :tabs="contentTabs"
       :active-tab="activeTab"
       @selectTab="scrollToTab"
     />
@@ -132,5 +159,39 @@ onUnmounted(() => {
 .desktop-layout .tab-panel {
   display: block;
   flex: 1 1 0;
+}
+
+.main-header {
+  width: 100%;
+  z-index: 2000;
+  background: $bgPrimary;
+  color: $txPrimary;
+
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  padding: $xxxxs $md;
+  border-bottom: 1px solid $boxPrimaryOutline;
+
+  a {
+    color: $txPrimary;
+    text-decoration: none;
+    opacity: 0.8;
+    padding: $xxxxs $xxs;
+    &.active {
+      opacity: 1;
+      font-weight: bold;
+      border-bottom: 2px solid $txPrimary;
+    }
+    &:hover {
+      opacity: 1;
+      text-decoration: underline;
+    }
+  }
 }
 </style>
