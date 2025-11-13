19f957c03e4a9c5c6b9f783b57ec5d42da62637b
Juhana Autti <juhana.autti@gmail.com>
2025-11-13 13:36:31 +0200

    add requirementsCheck with context

--- frontend/src/composables/useRequirements.js
+++ frontend/src/composables/useRequirements.js
@@ -3,23 +3,40 @@ import { useActivityStore } from "@/store/activity";
 import { useGearStore } from "@/store/gear";
 import { usePlayerStore } from "@/store/player";
 import { useItemsStore } from "@/store/items";
+import { useRouteStore } from "@/store/route";
 
 export function useRequirements() {
   const activity = useActivityStore();
   const gear = useGearStore();
   const player = usePlayerStore();
   const items = useItemsStore();
+  const route = useRouteStore();
 
-  const checkRequirements = (reqs) => {
-    if (!reqs || !reqs.length) return true;
-
-    const data = {
+  const getRequirementsContext = () => {
+    return {
       activity: activity.activitySelected && activity.activity,
       recipe: activity.recipeSelected && activity.recipe,
       location: activity.location,
       achievementPoints: player.achievementPoints,
       gear: gear.equippedGear,
       factionReputation: player.factionReputation,
+      segments: route.segments,
+    };
+  };
+
+  const checkRequirements = (reqs, context = {}) => {
+    if (!reqs || !reqs.length) return true;
+    const baseData = getRequirementsContext();
+    const data = {
+      activity: context.activity || baseData.activity,
+      recipe: context.recipe || baseData.receipe,
+      location: context.location || baseData.location,
+      achievementPoints:
+        context.achievementPoints || baseData.achievementPoints,
+      gear: context.equippedGear || baseData.gear,
+      factionReputation:
+        context.factionReputation || baseData.factionReputation,
+      segments: context.segments || baseData.segments,
     };
 
     return reqs.every((requirements) => checkRequirement(requirements, data));
@@ -34,6 +51,7 @@ export function useRequirements() {
       achievementPoints,
       gear,
       factionReputation,
+      segments,
     } = data;
     const equippedKeywordCounts = gear
       ? gear
@@ -69,12 +87,20 @@ export function useRequirements() {
         value = true;
         break;
       case "realm":
-        // TODO: add location check for travelling
-        if (activity) value = activity.id === "travelling";
         if (location)
           value =
             location.faction === requirement.realm ||
             location.subFactions?.includes(requirement.realm);
+        if (!location && activity) {
+          const factionInfo = segments.map(({ from }) => {
+            return { faction: from.faction, subFactions: from.subFactions };
+          });
+          value = factionInfo.some(
+            ({ faction, subFactions }) =>
+              faction === requirement.realm ||
+              subFactions?.includes(requirement.realm)
+          );
+        }
         break;
       case "traveling":
         if (activity) value = activity.id === "travelling";
@@ -97,6 +123,7 @@ export function useRequirements() {
           Object.values(player.skillLevels).reduce((a, b) => a + b - 1, 0) >=
           requirement.levels;
         break;
+      case "itemAnywhereWithYou":
       case "itemAnywhere":
         value = requirement.item in items.ownedItems;
         break;
@@ -108,6 +135,7 @@ export function useRequirements() {
   };
 
   return {
+    getRequirementsContext,
     checkRequirements,
   };
 }
