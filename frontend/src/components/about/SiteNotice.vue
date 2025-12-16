8b4551955b732c58eb8b907fa4d0032a7331b670
Juhana Autti <juhana.autti@gmail.com>
2025-12-16 10:42:32 +0200

    add SiteNotice

---
+++ frontend/src/components/about/SiteNotice.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref } from "vue";
+
+const showNotice = ref(true);
+const noticeHidden = localStorage.getItem("hideNotice");
+showNotice.value = !noticeHidden;
+
+function hideNotice() {
+  showNotice.value = false;
+  localStorage.setItem("hideNotice", true);
+}
+</script>
+
+<template>
+  <div v-if="showNotice" class="notice">
+    <div></div>
+    <p>Wiki Embargo in place. New items hidden unless imported from in-game.</p>
+    <button class="close-button" @click="hideNotice" type="button">✕</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.notice {
+  position: fixed;
+  left: 0;
+  top: $navHeight;
+  height: $navHeight;
+  width: 100%;
+
+  box-sizing: border-box;
+  z-index: 2000;
+  background: $txNegativeDark;
+  color: $txPrimary;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: $xxxxs $md;
+  border-bottom: 1px solid $boxPrimaryOutline;
+}
+
+.close-button {
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: $base;
+  width: $xxlg;
+  height: $xxlg;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: $xxs;
+  border: 1px solid $boxDarkOutline;
+
+  &:hover {
+    color: $txPrimary;
+    background-color: $boxTransparentDarkOutline;
+  }
+}
+</style>
