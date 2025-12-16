3bfe9420aefca7074e51f211abc55b004da84e4d
Juhana Autti <juhana.autti@gmail.com>
2025-12-16 13:46:26 +0200

    hide wallisia/wrentmark reputation

--- frontend/src/store/player.js
+++ frontend/src/store/player.js
@@ -48,8 +48,13 @@ export const usePlayerStore = defineStore("playerStore", {
       this.setAchievementPoints(playerStats.achievementPoints ?? 0);
 
       this.factions = factions.sort((a, b) => a.name.localeCompare(b.name));
+      const hiddenReputation = new Set([
+        "wallisiaReputation",
+        "wrentmarkReputation",
+      ]);
       this.reputationFactions = this.factions.filter(
-        ({ reputation }) => reputation !== null
+        ({ reputation }) =>
+          reputation !== null && !hiddenReputation.has(reputation)
       );
       this.factionReputation = Object.fromEntries(
         factions.map(({ reputation }) => [
