1f8b22a5d1fd00923afd5150c3b7842c7b9b830a
Juhana Autti <juhana.autti@gmail.com>
2025-12-17 08:06:50 +0200

    fix service filtering

--- frontend/src/utils/services.js
+++ frontend/src/utils/services.js
@@ -1,15 +1,18 @@
 export const serviceTiers = ["basic", "advanced", "expert"];
 
 export const filterServices = (services, requirement) => {
-  const keywords = requirement.keywords.map((kw) =>
-    kw.startsWith("service_") ? kw.slice(8) : kw
-  );
+  const kws =
+    "keywords" in requirement && requirement["keywords"].length
+      ? requirement.keywords.map((kw) =>
+          kw.startsWith("service_") ? kw.slice(8) : kw
+        )
+      : [];
 
   return services.filter((service) => {
     const checkTier =
       serviceTiers.indexOf(service.tier) >=
       serviceTiers.indexOf(requirement.tier);
-    const checkKeywords = keywords.every((kw) => service.keywords.includes(kw));
+    const checkKeywords = kws.every((kw) => service.keywords.includes(kw));
     return checkTier && checkKeywords;
   });
 };
