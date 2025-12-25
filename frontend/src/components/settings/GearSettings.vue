9b216d85743e914a6366b813282e6655b348cdf3
Juhana Autti <juhana.autti@gmail.com>
2025-12-25 09:24:46 +0200

    fix gearSettings display saving

--- frontend/src/components/settings/GearSettings.vue
+++ frontend/src/components/settings/GearSettings.vue
@@ -9,7 +9,7 @@ function updateSettingValue(key, value) {
 }
 
 function updateSettingDisplay(key, display) {
-  settingsStore.markSettingChanged(key, undefined, display);
+  settingsStore.markSettingChanged(key, undefined, display ? 1 : 0);
 }
 </script>
 
