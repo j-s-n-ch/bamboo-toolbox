1869268bf736fd92c09076897c10276b35162b18
Juhana Autti <juhana.autti@gmail.com>
2026-01-16 11:46:45 +0200

    Simplify DropStepColumn

--- frontend/src/components/activity/comparison/table/DropStepColumn.vue
+++ frontend/src/components/activity/comparison/table/DropStepColumn.vue
@@ -2,9 +2,6 @@
 import { storeToRefs } from "pinia";
 import { useSettingsStore } from "@/store/settings";
 import DropStepValue from "./DropStepValue.vue";
-import WsIcon from "@/components/common/WsIcon.vue";
-import { icons } from "@/constants/iconPaths";
-import { n } from "@/utils/number";
 
 defineProps({
   data: Object,
@@ -19,26 +16,18 @@ const { activitySettings } = storeToRefs(settingsStore);
 <template>
   <td>
     <div v-if="data.item > 0" class="step-counts">
-      <div class="steps-line border-common">
-        <span
-          v-if="activitySettings.shownDropRate.display === 1"
-          :class="{
-            positive: invert ? item.normalComp > 0 : item.normalComp < 0,
-            negative: invert ? item.normalComp < 0 : item.normalComp > 0,
-          }"
-          >{{ n(data.normal, data.normal < 100 ? 1 : 0) }}</span
-        >
-        <span
-          v-else
-          :class="{
-            positive: invert ? item.comp > 0 : item.comp < 0,
-            negative: invert ? item.comp < 0 : item.comp > 0,
-          }"
-          >{{ n(data.item, data.item < 100 ? 1 : 0) }}</span
-        >
-        <ws-icon :iconPath="icons.steps" size="xs" />
-      </div>
-
+      <drop-step-value
+        class="border-common"
+        :data="
+          activitySettings.shownDropRate.display === 1 ? data.normal : data.item
+        "
+        :comp="
+          activitySettings.shownDropRate.display === 1
+            ? item.normalComp
+            : item.comp
+        "
+        :invert="invert"
+      />
       <drop-step-value
         class="border-fine"
         :data="data.fine"
